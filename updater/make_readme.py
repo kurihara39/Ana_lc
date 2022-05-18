78d72d76e455f6b7a321a4e16a5d6429a9738424
Miki Kurihara <kurihara@ac.jaxa.com>
2022-05-18 18:16:51 +0900

    otoro

--- updater/make_readme.py
+++ updater/make_readme.py
@@ -5,7 +5,7 @@ path = f"/data01/kurihara/Ana_lc/{today}"
 
 files = glob.glob(f"{path}/*.html")
 
-with open(f"{path}/README_{today}.txt", "w") as f:
+with open(f"{path}/README.txt", "w") as f:
     print("## github home", file=f)
     print("https://github.com/kurihara39/Ana_lc", file=f)
     print("", file=f)
@@ -19,4 +19,4 @@ with open(f"{path}/README_{today}.txt", "w") as f:
     for i in files:
         print(i.replace("/data01/kurihara", "https://kurihara39.github.io"), file=f)
     
-    print(f"Finished writing on {path}/README_{today}.txt")
+    print(f"Finished writing on {path}/README.txt")
